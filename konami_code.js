bbae9a6eaee9d77748e4c5a8c2aa8755da26ca08
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-12 19:46:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,11 +19,10 @@ function init() {
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
-    index = (codes[index] === key) ? ++index : 0
+    index = (codes[index] === key) ? ++index: 0
     
     if (index === codes.length) {
       window.alert("Hurray!");
-     
     }
     
   });
